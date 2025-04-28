8baccb36b26443468d2998570f2c0b67aa0b88a2
braden <leachbr787@northwested.org>
2025-04-28 14:17:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+# 
+# 
+# 
 
+# -- 
+class Dog: 
+    "A atempt to model a dog"
+
+    def __init__(self, name, age): 
+        "init name and age"
+        self.name = name
+        self.age = age 
+
+    def sit(self):
+        print(f'{self.name} is now sitting.')
+
+    def roll_over(self): 
+        print(f'{self.name} rolled over!')
